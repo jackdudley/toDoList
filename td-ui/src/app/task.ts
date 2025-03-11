873cb2b31b701f2cef75d7e3efccb3794a305b55
jackdudley <jack.dudley340@gmail.com>
2025-03-11 13:18:58 -0400

    fixed alignment

--- td-ui/src/app/task.ts
+++ td-ui/src/app/task.ts
@@ -2,6 +2,7 @@ export interface Task {
     id: number;
     content: string;
     completed: boolean;
-    task_length: number;
+    task_length: number | null;
     dueDate: string;
+    description: string;
 }
